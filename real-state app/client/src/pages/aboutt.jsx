958bc140dd941cf77edf157fe6d80814bb43d4ab
Sachin Yadav <sachinyadav68763@gmail.com>
2024-10-08 14:28:38 +0530

    changes

--- real-state app/client/src/pages/aboutt.jsx
+++ real-state app/client/src/pages/aboutt.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const About = () => {
+const Aboutt = () => {
   return (
     <div>About</div>
   )
 }
 
-export default About
+export default Aboutt
--- real-state app/client/src/pages/profile.jsx
+++ real-state app/client/src/pages/profile.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const profile = () => {
+const Profile = () => {
   return (
-    <div>profile</div>
+    <div>P</div>
   )
 }
 
-export default profile
+export default Profile
