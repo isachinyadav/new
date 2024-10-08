b4407f4031b6fe38044d96058afeb919025c21d4
Sachin Yadav <sachinyadav68763@gmail.com>
2024-10-08 13:35:10 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import Signin from './pages/siignin';
 import SignOut from './pages/signoout';
 import About from './pages/aboutt';
 import Profile from './pages/profile';
-
+import Header  from './components/header';
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/sign-in' element={<Signin/>}/>
